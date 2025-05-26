34270aaaf355020de989c00df1f7c624098dc54d
Vishal Row Mysore <visrow@gmail.com>
2025-05-26 17:55:31 -0400

    refactor: update A2A web browsing agent and controllers for improved structure and functionality

--- src/main/java/io/github/vishalmysore/SeleniumTaskController.java
+++ src/main/java/io/github/vishalmysore/SeleniumTaskController.java
@@ -2,14 +2,30 @@ package io.github.vishalmysore;
 
 import com.t4a.processor.scripts.BaseScriptProcessor;
 import com.t4a.processor.scripts.SeleniumScriptProcessor;
+import com.t4a.processor.selenium.SeleniumOpenAIProcessor;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import io.github.vishalmysore.a2a.server.DyanamicTaskContoller;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SeleniumTaskController extends DyanamicTaskContoller {
     BaseScriptProcessor baseScriptProcessor;
     public SeleniumTaskController() {
-        this.baseScriptProcessor = new SeleniumScriptProcessor();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new"); // or just "--headless" if < Chrome 109
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--disable-software-rasterizer");
+        options.addArguments("--disable-extensions");
+        options.addArguments("--remote-debugging-pipe");
+        options.addArguments("--window-size=1920,1080");
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver(options);
+        this.baseScriptProcessor = new SeleniumScriptProcessor(new SeleniumOpenAIProcessor(driver));
 
     }
 
--- src/main/java/io/github/vishalmysore/WebBrowsingAction.java
+++ src/main/java/io/github/vishalmysore/WebBrowsingAction.java
@@ -11,8 +11,13 @@ import com.t4a.processor.OpenAiActionProcessor;
 import com.t4a.processor.scripts.ScriptProcessor;
 import com.t4a.processor.scripts.ScriptResult;
 import com.t4a.processor.scripts.SeleniumScriptProcessor;
+import com.t4a.processor.selenium.SeleniumOpenAIProcessor;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import io.github.vishalmysore.a2a.server.SSEEmitterCallback;
 import lombok.extern.java.Log;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -25,6 +30,22 @@ import java.nio.file.StandardCopyOption;
 @Log
 @Agent(groupName = "web browsing", groupDescription = "actions related to web browsing and searching and validation of web pages ")
 public class WebBrowsingAction {
+    SeleniumScriptProcessor script;
+    public WebBrowsingAction(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new"); // or just "--headless" if < Chrome 109
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--disable-software-rasterizer");
+        options.addArguments("--disable-extensions");
+        options.addArguments("--remote-debugging-pipe");
+        options.addArguments("--window-size=1920,1080");
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver(options);
+        script = new SeleniumScriptProcessor(new SeleniumOpenAIProcessor(driver));
+    }
+
     private ActionCallback callback;
     private AIProcessor processor;
     @Action(description = "perform actions on the web with selenium and return text")
@@ -36,7 +57,7 @@ public class WebBrowsingAction {
                 StringBuffer seperatedWebBrowsingSteps = new StringBuffer(processor.query("Separate the web browsing steps into individual steps  just give me steps without any additional text or brackets {"+ webBrowsingSteps+"}"));
                 //you can create your own selenium processor which implements SeleniumProcessor
                 //SeleniumScriptProcessor script = new SeleniumScriptProcessor(new MyOwnSeleniumScriptProcessor());
-                SeleniumScriptProcessor script = new SeleniumScriptProcessor();
+
                 script.process(seperatedWebBrowsingSteps,seleniumCallBack);
                                 return result.getLastData();
             } catch (AIProcessingException e) {
@@ -56,7 +77,7 @@ public class WebBrowsingAction {
         if(processor != null) {
             try {
                 StringBuffer seperatedWebBrowsingSteps = new StringBuffer(processor.query("Separate the web browsing steps into individual steps  just give me steps without any additional text or brackets {"+ webBrowsingSteps+"}"));
-                SeleniumScriptProcessor script = new SeleniumScriptProcessor();
+
                 script.process(seperatedWebBrowsingSteps,seleniumCallBack);
                 return result.getLastScreenshotAsBase64();
             } catch (AIProcessingException e) {
